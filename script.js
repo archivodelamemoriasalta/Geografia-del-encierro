a3f2d812d1014560405c672157ef52dc6a9ce24f
Archivo de la memoria <archivodelamemoriasalta@gmail.com>
2026-02-05 12:48:10 -0300

    Mostrar Departamentos Salta

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+// Crear el mapa
 const map = L.map('map').setView([-24.8, -65.4], 7);
 
+// Capa base
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap'
 }).addTo(map);
+
+// Cargar departamentos
+fetch('salta_departamentos.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: {
+        fillColor: '#dddddd',
+        weight: 1,
+        color: '#555',
+        fillOpacity: 0.7
+      }
+    }).addTo(map);
+  });
